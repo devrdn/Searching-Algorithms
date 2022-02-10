c7b2e54a794ebb93d627e0c665d0bf2406f2ee0b
/rdndev <rdninc@mail.ru>
2022-02-11 00:56:39 +0200

    new algorithm

---
+++ algorithms/InterpolationSearch.cpp
@@ -0,0 +1,43 @@
+#include "../include/Header.h"
+
+/*
+* arr - массив объектов класса Employee.
+* count - количество элементов в массиве.
+* key - ключ поиска.
+*/
+int InterpolationSearch(Employee* arr, int count, int key) {
+	int left = 0;  int right = count - 1; // Индексы конца и начала массива.
+	int it_count = 0; // Счетчик итераций.
+
+	// Левая сторона отрезка, должна быть меньше, чем левая.
+	// Ключ должен находится в пределах определенных отерзком.
+	while (left <= right && key >= arr[left].id && key <= arr[right].id) {
+		it_count++;
+		if (left == right) {
+			cout << "Count of iterrations: " << it_count << endl;
+			if (arr[left].id == key)
+				return left;
+			return -1;
+		}
+
+		// Считаем позицию по формуле интерполяции.
+		int position = left + (((double)(right - left) / (arr[right].id - arr[left].id)) * (key - arr[left].id));
+
+		// Ключ был найден в массиве.
+		if (arr[position].id == key) {
+			cout << "Count of iterrations: " << it_count << endl;
+			return position;
+		}
+		
+		// Ключ находится после текущей позиции.
+		if (arr[position].id < key)
+			left = position + 1;
+
+		// Ключ находится перед текущей позицией.
+		else
+			right = position - 1;
+	}
+
+	// Если сотрудник не был найден.
+	return -1;
+}
