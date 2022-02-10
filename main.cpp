61bdf78be1828c5a77f20937ee8c0e4eb4a6d258
/rdndev <rdninc@mail.ru>
2022-02-11 00:26:02 +0200

    new struct main.cpp

--- main.cpp
+++ main.cpp
@@ -10,60 +10,68 @@ void printArray(Employee* emp, int count_emp);
 int main()
 {
 
+	/* Ключи поиска: числовой и строковый */
+	const int number_key = 200; 
+	const string string_key = "Melloni Tea"; 
+
+	/* Пути к файлам со списком */
+	const string inord_path = "data/inordered.txt"; // путь к файлу с неотсортированными данным.
+	const string ord_path = "data/ordered.txt"; // уть к файлу с отсортированными данным.
+
 	/* Инициализация переменных */
-	const int number_key = 200; // численный ключ.
-	const string string_key = "Melloni Tea"; // строковый ключ.
 	int id = -1; // возвращаемый номер позиции.
+	int it_count = 0; // количество иттераций для поиска в бинарном.
 	char flag; // флаг, для выбора меню.
-	const string inord_path = "data/inordered.txt"; // путь к неотсортированным данным.
-	const string ord_path = "data/ordered.txt"; // пусть к сортированным данным.
 	ifstream out(inord_path); // объект класса ifstream  (входной файловый поток).
 	int count_emp = count_file_lines(out) - 1; // количество записй в файле.
-	Employee* emp = new Employee[count_emp]; // инициализируется динамический массив объектов класса Employee.
+
+	/* Инициализируем массив который будет содержать отсортированные данные. */
+	Employee* emp_ord = new Employee[count_emp];
+	outFileArray(out, ord_path, emp_ord, count_emp);
+
+	/* Инициализируем массив который будет содержать неотсортированные данные. */
+	Employee* emp_inord = new Employee[count_emp];
+	outFileArray(out, inord_path, emp_inord, count_emp);
+
+	/* Инициализируем бинарное дерево, содержанее данные */
+	Tree tree = nullptr;
+	createTree(out, inord_path, tree, count_emp);
+
 
 	/* Меню выбора алгоритма */
 	do {
 		system("cls");
-		cout << "Methods: " << endl << endl;
+		cout << "Select Method: " << endl << endl;
 		cout << " 1. Sequential Search." << endl;
 		cout << " 2. Binary Search." << endl;
 		cout << " 3. Binary Tree Search." << endl;
 		cout << " 4. Exit." << endl;
 		cout << "> ";
-		flag = getchar();
-		if (emp != nullptr) {
-			delete[] emp;
-			emp = new Employee[count_emp];
-		}
+		flag = _getche();
 		system("cls");
 		switch (flag) {
 			case '1':
 			{
-				/* Последовательный поиск */
+				/* Последовательный поиск в неупорядоченной таблице */
 				cout << " > InOrdered List of Employee: " << endl;
-				outFileArray(out, inord_path, emp, count_emp);
-				printArray(emp, count_emp);
-				id = SequentialSearch(emp, count_emp, number_key);
-				cout <<  "Searched Employee: " << ((id == -1) ? "Unknown Employee." : emp[id].toString());
+				printArray(emp_inord, count_emp);
+				id = SequentialSearch(emp_inord, count_emp, number_key);
+				cout << "Searched Employee: " << ((id == -1) ? "Unknown Employee." : emp_inord[id].toString());
 				break;
 			}
 			case '2':
 			{
-				/* Бинарный поиск */
+				/* Бинарный поиск в упорядоченной таблице */
 				cout << " > Ordered List of Employee: " << endl;
-				outFileArray(out, ord_path, emp, count_emp);
-				printArray(emp, count_emp);
-				id = BinarySearch(emp, count_emp, number_key);
-				cout << "Searched Employee: " << ((id == -1) ? "Unknown Employee." : emp[id].toString());
+				printArray(emp_ord, count_emp);
+				id = BinarySearch(emp_ord, count_emp, number_key);
+				cout << "Searched Employee: " << ((id == -1) ? "Unknown Employee." : emp_ord[id].toString());
 				break;
 			}
 			case '3':
 			{
-				// Инициализируем бинарное дерево (ключем является имя человека).
-				Node* tree = nullptr; // Дерево.
-				Node* result = nullptr; // Результат поиска.
-				int it_count = 0; // Количество иттераций.
-				createTree(out, inord_path, tree, count_emp); 
+				/* Поиск в бинарном дереве */
+				Tree result = nullptr; // Результат поиска.
 				cout << endl << endl << " > List of Emoloyee (Tree): " << endl;
 				printTree(tree);
 				cout << endl << endl << " > Binary Tree Search: ";
@@ -72,18 +80,26 @@ int main()
 					cout << "Unknown Employee.";
 				else
 					cout << endl << "Searched Employee: " << result->emp.toString() << endl << "Count of iterrations: " << it_count << endl;
-				deleteTree(tree);
 				break;
 			}
 			case '4':
-				exit(0);
+			{
 				break;
+			}
+			default:
+			{
+				cout << "Select correct option: 1-5";
+				break;
+			}
 		}
-		system("pause > null");
-	} while (true);
+		system("pause > nul");
+	} while (flag != '4');
+
+	delete[] emp_ord;
+	delete[] emp_inord;
+	deleteTree(tree);
 
 	system("pause > nul");
-	
 }
 
 /*  Вывод массива объектов Employee. 
@@ -137,5 +153,18 @@ void outFileArray(ifstream& out, string path, Employee* emp, int count_emp) {
 	}
 	out.close();
 }
+
+class ArinaLucshayaDevushka {
+	int ArinaLove;
+public:
+	ArinaLucshayaDevushka() {
+		ArinaLove = 1;
+	}
+	ArinaLucshayaDevushka(int ArinaLove) {
+		this->ArinaLove = ArinaLove;
+	}
+};
+
+
 
 
